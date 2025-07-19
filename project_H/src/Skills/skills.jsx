48f3755dcdd420d47342408804044f7f8f388033
shabebe11 <salk147@aucklanduni.ac.nz>
2025-07-19 12:30:28 +1200

    Done skills and educaton sjsx files

---
+++ project_H/src/Skills/skills.jsx
@@ -0,0 +1,17 @@
+import './Education.css'
+
+
+export function Education({ educationData }) {
+    return (
+        <div className="educations_total">
+            {educationData.map((education, index) => (
+                <div key={index} className="individual_education">
+                    <h2 className="school_name">{education.school_name}</h2>
+                    <h4 className="degree_name">{education.degree_name}</h4>
+                    <h4 className="dates">{education.start_date} - {education.end_date}</h4>
+                    <p>{education.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
