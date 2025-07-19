d9c4494bfd4ec0508fd4e38823536373b72415d8
shabebe11 <salk147@aucklanduni.ac.nz>
2025-07-19 12:53:51 +1200

    Saving work in the skills branch

--- project_H/src/Skills/skills.jsx
+++ project_H/src/Skills/skills.jsx
@@ -1,15 +1,16 @@
-import './Education.css'
+import './skills.css'
 
 
-export function Education({ educationData }) {
+
+export function skills({ skillsData }) {
     return (
-        <div className="educations_total">
-            {educationData.map((education, index) => (
-                <div key={index} className="individual_education">
-                    <h2 className="school_name">{education.school_name}</h2>
-                    <h4 className="degree_name">{education.degree_name}</h4>
-                    <h4 className="dates">{education.start_date} - {education.end_date}</h4>
-                    <p>{education.description}</p>
+        <div className="skills_total">
+            <h2 className="skills_title">My skills:</h2>
+            {skillsData.map((skills, index) => (
+                <div key={index} className="individual_skills">
+                    <h3 className="skill_name">{skills.degree_name}</h3>
+                    <h4 className="skill_level">{skills.start_date} - {skills.end_date}</h4>
+                    <p>{skills.description}</p>
                 </div>
             ))}
         </div>
