8d45303a48484521f034faf651170740209a3825
Manit Kc <mkc184@aucklanduni.ac.nz>
2025-07-19 13:23:25 +1200

    AboutUS page done

--- project_H/src/App.jsx
+++ project_H/src/App.jsx
@@ -2,24 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AboutUs } from './AboutUs/AboutUs'
 
-function App({Aboutme} ) {
-
-  console.log(Aboutme)
-  
+function App( ) {
 
   return (
-      <div>
-      {Aboutme.map(person => (
-        <div >
-          <h3>{person.name}</h3>
-          <p>{person.dateOfBirth}</p>
-          <p>{person.school}</p>
-           <p>{person.description}</p>
-        </div>
-      ))}
-    </div>
-   
+   <AboutUs/>
   )
 }
 
--- project_H/src/main.jsx
+++ project_H/src/main.jsx
@@ -4,30 +4,9 @@ import './index.css'
 import App from './App.jsx'
 
 
-const Aboutme = [
-  {
-    name:"Manit ",
-    dateOfBirth: "27/09/2005",
-    school: "Auckland University",
-    description: "Studying software enginnering at Uoa"
-
-  },
-  {
-
-     name:"Dave ",
-    dateOfBirth: "15/02/2005",
-     School: "Auckkland University",
-     description: "Studying software enginnering at Uoa"
-
-  }
-
-]
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App
-    Aboutme = {Aboutme}
-     />
+    <App/>
   </StrictMode>,
 )
