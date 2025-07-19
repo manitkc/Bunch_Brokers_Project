734ddd88951f05aa3585f6fc7b757e6040e2b59e
stapper30 <stap328@aucklanduni.ac.nz>
2025-07-19 17:45:07 +1200

    styled the register and login

--- project_H/src/App.jsx
+++ project_H/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Login from './Login/Login.jsx';
 import HomePage from "./HomePage/HomePage.jsx";
+import Registration from "./Registration/Registration.jsx";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/Registration" element={<Registration />} />
       </Routes>
     </div>
 
