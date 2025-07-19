ce1373ad2970ae91127e116e9f4cb105b3e31778
shabebe11 <salk147@aucklanduni.ac.nz>
2025-07-19 13:13:15 +1200

    Savign work in current branch

--- project_H/src/Skills/skills.jsx
+++ project_H/src/Skills/skills.jsx
@@ -3,16 +3,16 @@ import './skills.css'
 
 
 export function skills({ skillsData }) {
-    return (
-        <div className="skills_total">
-            <h2 className="skills_title">My skills:</h2>
-            {skillsData.map((skills, index) => (
-                <div key={index} className="individual_skills">
-                    <h3 className="skill_name">{skills.degree_name}</h3>
-                    <h4 className="skill_level">{skills.start_date} - {skills.end_date}</h4>
-                    <p>{skills.description}</p>
-                </div>
-            ))}
+  return (
+    <div className="skills_total">
+      <h2 className="skills_title">My skills:</h2>
+      {skillsData.map((skills, index) => (
+        <div key={index} className="individual_skills">
+          <h3 className="skill_name">{skills.degree_name}</h3>
+          <h4 className="skill_level">{skills.start_date} - {skills.end_date}</h4>
+          <p>{skills.description}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
